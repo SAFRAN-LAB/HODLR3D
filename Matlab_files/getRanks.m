c3f28bb4a493b17d0acc2f51a88021574d1739ad
Vaishnavi <vaishnavihp@gmail.com>
2023-05-19 23:51:14 +0530

    Matlab_files modified

--- Matlab_files/getRanks.m
+++ Matlab_files/getRanks.m
@@ -1,6 +1,14 @@
 clc;
 clear all;
 close all;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% For the data loaded from a given file, the code outputs the numerical
+% rank for a given tolerance of 'tolr'
+% OUTPUT: 'r_f' numerical rank of the face sharing interaction, 'r_e' numerical rank of the edge sharing
+% interaction, 'r_v' numerical rank of the vertex sharing interaction, 'r_w' numerical rank of the well-separated interaction.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 data = load('output_file_125_3.mat');
 tolr = 1e-14;
 r_f = numerical_rank(data.svd_f,tolr)
